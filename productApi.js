03b97e233790bd12da197d5eff557292e1077dbc
Atharvan2004 <123322068+Atharvan2004@users.noreply.github.com>
2023-09-01 00:55:18 +0530

    Update productApi.js login & register
    Added login and register routes fully working with password hashing.

--- productApi.js
+++ productApi.js
@@ -2,9 +2,12 @@ import mongoose, { connect, Schema, model, mongo } from "mongoose";
 import express from "express";
 import axios from "axios";
 import * as cheerio from 'cheerio';
- 
+import bcrypt from "bcrypt"
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 4000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port,()=>{
     console.log("Server started port 4000");
@@ -29,11 +32,22 @@ const productSchema = new Schema({
     imageURL:String
 })
 
+const userSchema = new Schema({
+    userName:String,
+    password:String,
+    email:String
+})
+
+const Users = model("user",userSchema);
 //assign headphones variable to the headphones collection
 // const headphones = model("headphones",productSchema);
 
 // const headphone1 = new Headphone({"title":"Redgear Cosmo 7,1 Usb Gaming Wired Over Ear Headphones With Mic With Virtual Surround Sound,50Mm Driver, Rgb Leds & Remote Control(Black)","price":""," colour":"Black)","discount":"","features":["Redgear Cosmo 7","50Mm Driver"," Rgb Led s & Remote Control(Black)"],"href":"/Redgear-Cosmo-7-1-Headphones-Controller/dp/B079S811J3/ref=sr_1_1?keywords=gaming+headphones&qid=1693218774&sr=8-1","review":" 16,164"});
-
+Users.find().then((users)=>{
+    users.forEach((user)=>{
+        console.log(user)
+    })
+})
 
 
 //find and log all data of particular collection
@@ -56,7 +70,7 @@ async function productDetails(productId){
     var search = model(models[index] ,productSchema);
     await search.find().then((items)=>{
         items.forEach(element => {
-           
+    
         if(String(element._id)==String(productId)){
              item = element
         }
@@ -84,9 +98,62 @@ app.get("/product/:id",async(req,res)=>{
     res.render('index.ejs',{content:product});
 })
 
+app.get("/register",async(req,res)=>{
+    res.render('register.ejs')
+})
 app.get("/login",async(req,res)=>{
     res.render('login.ejs')
 })
 app.get("/cart",async(req,res)=>{
     
 })
+
+app.post("/register",async(req,res)=>{
+    console.log(req.body)
+    var hashed;
+        try {
+            const userName = req.body.userName
+            const password = req.body.password
+            const email = req.body.email
+            const user =  Users.findOne({ userName: userName ,email:email});
+            if(user){
+                res.render('register.ejs',{message:"User exists, try logging in"});
+            }
+            else{
+                bcrypt.hash(password,10).then((hash)=>{
+                hashed=hash;})
+                Users.create({userName:userName,password:hashed,email:email});
+                res.render('register.ejs',{message:"register success"});
+            }
+            
+        } 
+        catch (error) {
+            res.render('register.ejs',{message:"register failed"+error});
+        }
+    
+});
+
+app.post("/login",async(req,res)=>{
+    console.log(req.body)
+    var hashed;
+    try {
+        const userName = req.body.userName
+        const password = req.body.password
+        const email = req.body.email
+        const user =  Users.findOne({ userName: userName });
+        if(user){
+            bcrypt.hash(password,10).then((hash)=>{
+                hashed=hash;
+            })
+            if(hashed==user.password)
+            {
+                res.render('login.ejs',{message:"User successfully logged in"});
+            }
+            else{
+                res.render('login.ejs',{message:"Login failed"});
+            }
+        }
+    }catch(error){
+        res.render('login.ejs',{message:"Login error : "+error});
+    }
+});
