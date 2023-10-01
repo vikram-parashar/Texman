0a057417b82489a52171dda2f635905fa1a9fb68
AstikSharma <astksharma26@gmail.com>
2023-10-01 22:44:16 +0530

    qwe

--- public/scripts/product-page.js
+++ public/scripts/product-page.js
@@ -44,5 +44,7 @@ function openCity(evt, cityName) {
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  if(evt) evt.currentTarget.className += " active";
+    else document.querySelector('button.tablinks').className += " active";
 }
+document.body.addEventListener('DOMContentLoaded', openCity(event, 'London'));
